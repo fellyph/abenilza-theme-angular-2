e2e2c4e34d5a872b4b58c6149b9401e6e9af50ee
fellyph <fellyph.cintra@gmail.com>
2017-12-28 15:16:51 +0000

    adding client component

---
+++ src/app/client/client-item-component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-client-item',
+  templateUrl: 'app/client/client-item-component.html',
+  styleUrls: ['app/client/client-item.component.css']
+})
+
+export class MediaItemComponent { }
